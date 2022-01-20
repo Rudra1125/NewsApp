80fefb39a16a7c9a115e9f7144eb9e61a1ca538a
Rudra1125 <shankarrudra36@gmail.com>
2022-01-20 18:35:23 +0530

    Api fetch applied useState constructer

--- src/NewsItem.js
+++ src/NewsItem.js
@@ -1,9 +1,20 @@
+// import { getByTitle } from '@testing-library/react' 
 import React, { Component } from 'react';
 
 export class NewsItem extends Component {
-  render() {
-    return <div>
-        this is a news item
+    
+  
+    render() {
+    let {title, description, imageUrl,newsUrl }=this.props;
+    return <div className='my-3'>
+        <div className="card" style= {{width: "18rem"}}>
+            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <a href="/newsDetail/" className="btn btn-primary btn-sm">Read More</a>
+            </div>
+        </div>
     </div>;
   }
 }
