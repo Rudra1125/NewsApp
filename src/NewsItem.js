c78d19724db00c2c94b9afffa7e1b81053d96afd
Rudra1125 <shankarrudra36@gmail.com>
2022-01-20 21:08:00 +0530

    Fetch the API using componentDidMount

--- src/NewsItem.js
+++ src/NewsItem.js
@@ -8,7 +8,7 @@ export class NewsItem extends Component {
     let {title, description, imageUrl,newsUrl}=this.props;
     return <div className='my-3'>
         <div className="card" style= {{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={!imageUrl?"https://fdn.gsmarena.com/imgroot/news/22/01/realme-9-pro-plus-teaser-and-leak/-952x498w6/gsmarena_000.jpg":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
--- src/components/News.js
+++ src/components/News.js
@@ -2,67 +2,37 @@ import React, { Component } from 'react';
 import NewsItem from '../NewsItem';
 
 export class News extends Component {
-    articles =  [
-        {
-          "source": {
-            "id": "news24",
-            "name": "News24"
-          },
-          "author": "Compiled by Lloyd Burnard",
-          "title": "Proteas coach Boucher charged with misconduct following racism allegations, job now on the line",
-          "description": "Proteas head coach Mark Boucher has been charged with \"gross misconduct\" by Cricket South Africa (CSA) for his role in the alleged racial discrimination experienced by former national spinner Paul Adams.",
-          "url": "https://www.news24.com/sport/Cricket/Proteas/proteas-coach-boucher-charged-with-gross-misconduct-following-racism-allegations-job-on-the-line-20220120",
-          "urlToImage": "https://cdn.24.co.za/files/Cms/General/d/2128/d8eb0b31acc84f34972fcb3a755dbce9.jpg",
-          "publishedAt": "2022-01-20T15:21:18+00:00",
-          "content": "Proteas head coach Mark Boucher has been charged with \"gross misconduct\" by Cricket South Africa (CSA) for his alleged role in the racial discrimination experienced by former national spinner Paul Ad… [+1924 chars]"
-        },
-        {
-          "source": {
-            "id": "espn-cric-info",
-            "name": "ESPN Cric Info"
-          },
-          "author": null,
-          "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-          "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-          "publishedAt": "2020-04-27T11:41:47Z",
-          "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-          "source": {
-            "id": "espn-cric-info",
-            "name": "ESPN Cric Info"
-          },
-          "author": null,
-          "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-          "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-          "publishedAt": "2020-03-30T15:26:05Z",
-          "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-      ]
+    
+      
     
     constructor(){
         super();
         console.log("hello i am constructor from news component")
         this.state={
-            articles: this.articles,
+            articles: [],
             loading:false
             
         }
     }
+    async componentDidMount(){
+        console.log("cdm")
+        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=569d40835d1841eaacdf3495f5fee7b9";
+        let data =await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        this.setState({articles:parsedData.articles})
+    }
   
     render() {
+        console.log("render")
     return(
         <div className="container my-3">
-            <h2>Khabare-Top Headlines</h2>
+            <h1>Khabare-Top Headlines</h1>
             
             <div className="row">
             {this.state.articles.map((element)=>{
                 return <div className="col-md-4" key={element.url}>
-                    <NewsItem  title={element.title.slice(0,45)} description={element.description.slice(0,88)} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                    <NewsItem  title={element.title?element.title.slice(0,45):""} description={ element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
                 </div>
             })}
                 
