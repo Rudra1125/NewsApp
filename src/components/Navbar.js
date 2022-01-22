755fe1cce2beb3cf30df35dcba54a0760bc9fac4
Rudra1125 <shankarrudra36@gmail.com>
2022-01-22 14:09:28 +0530

    make next button disabled, add loading image change navbar icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 
 import React, { Component } from 'react';
+import Logo from './Logo';
 
 export class Navbar extends Component {
   
 
   render() {
     return <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Khabare</a>
+    <a className="navbar-brand" href="/"><Logo/></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
